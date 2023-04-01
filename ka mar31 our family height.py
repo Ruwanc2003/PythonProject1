c3c715d3bafdd91c8f8272c69761257ebf52bd06
keerthisl <abeywickrama@gmail.com>
2023-04-01 11:52:03 +0530

    create 2nd file
    1apr Ruwan online

---
+++ ka mar31 our family height.py
@@ -0,0 +1,12 @@
+# Our family name & height list
+
+dictionery = {"Name":"Height",
+              "****":"*****",
+              "Keerthi": 176, 
+              "Anoma":175,
+                "HTA":173}
+
+for name in dictionery:
+    height = str(dictionery[name])
+    print((name) +"\t" + (height))
+
